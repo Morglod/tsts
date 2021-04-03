bf08ac75886f9eeb84269b616687f1b2aa018169
Кружков Владимир <morglod@gmail.com>
2021-04-03 04:06:59 +0300

    wip on __compilerJob

--- plugins/ts-transformer-overload/index.ts
+++ plugins/ts-transformer-overload/index.ts
@@ -1,59 +1,13 @@
 import * as ts from 'typescript';
-import * as http from 'http';
+import * as tsee from 'tsee';
 
-// function request(url: string): Promise<http.IncomingMessage> {
-//     return new Promise(resolve => {
-//         http.request(url, res => {
-//             resolve(res);
-//         });
-//     });
-// }
-
-// function syncAsync<T>(job: () => Promise<T>): T {
-//     let done = false;
-//     let value;
-
-//     job().then(x => {
-//         console.log('done');
-//         value = x;
-//         done = true;
-//     });
-
-//     while (!done) {
-//         console.log('step');
-//         atomicSleep(100);
-//     }
-
-//     return value;
-// }
-
-// console.log(
-//     'HELLO',
-
-//     syncAsync(() => {
-//         return new Promise(reso => {
-//             setTimeout(() => {
-//                 reso(100)
-//             }, 250);
-//         })
-//     })
-// );
+const KEYWORD_COMPILER_EVAL = '__compilerEval';
+const KEYWORD_COMPTIME = 'comptime';
+const KEYWORD_COMPILER_JOB = '__compilerJob';
 
 export function unescapeText(name: string) {
     // getText() == `'ts-transformer-overload'`
     return name.trim().replace(/(^('|"|`))|(('|"|`)$)/g, '');
-}
-
-export function importDecl_getModuleSpecifierName(node: ts.ImportDeclaration) {
-    // getText() == `'ts-transformer-overload'`
-    return unescapeText(node.moduleSpecifier.getText());
-}
-
-export function isImportSpecifier(node: ts.Node, name: string): node is ts.ImportSpecifier {
-    if (ts.isImportSpecifier(node)) {
-
-    }
-    return false;
 }
 
 function unsafeIsNode(x: any): x is ts.Node {
@@ -79,19 +33,19 @@ function deepSynthesized(node: any) {
 // https://github.com/madou/typescript-transformer-handbook
 // https://github.com/Microsoft/TypeScript/blob/master/src/services/findAllReferences.ts
 
-function parseCode(code: string) {
+function parseCode(code: string): ts.SourceFile {
     const source = ts.createSourceFile('_generated_' + Date.now() + '.ts', code, ts.ScriptTarget.ES2020, false, ts.ScriptKind.TS);
     deepSynthesized(source);
     return source;
 }
 
-function parseCodePickExpression(code: string) {
+function parseCodePickExpression(code: string): ts.Node {
     const source = parseCode(code);
     return (source.statements[0] as any).expression;
 }
 
 /** attention! it will return function call expression, so you need to place it right */
-function parseFuncBodyAndGenerateInvoke(codeBody: string, ) {
+function parseFuncBodyAndGenerateInvoke(codeBody: string): ts.Node {
     const code = `(() => { ${codeBody} })()`;
     return parseCodePickExpression(code);
 }
@@ -115,7 +69,12 @@ function extractComptimeCode(checker: ts.TypeChecker, ctx: ts.TransformationCont
     }
 
     function visitor(node: ts.Node): ts.VisitResult<ts.Node> {
-        if (ts.isCallExpression(node) && ts.isIdentifier(node.expression)) {
+        if (
+            ts.isCallExpression(node) &&
+            ts.isIdentifier(node.expression) &&
+            unescapeText(node.expression.getText()) !== KEYWORD_COMPILER_EVAL &&
+            unescapeText(node.expression.getText()) !== KEYWORD_COMPILER_JOB
+        ) {
             const sign = checker.getResolvedSignature(node);
             const decl = sign.getDeclaration();
 
@@ -151,102 +110,234 @@ function extractComptimeCode(checker: ts.TypeChecker, ctx: ts.TransformationCont
     });
 
     if (!node) return undefined;
-    return printer.printNode(ts.EmitHint.Unspecified, node, node.getSourceFile());
+
+    let printed;
+    
+    try {
+        printed = printer.printNode(ts.EmitHint.Unspecified, node, node.getSourceFile());
+    } catch (err) {
+        console.error('failed print node:');
+        console.error(node.getFullText());
+        console.error('identifiers mapping:');
+        console.error(identifiersToCode.entries());
+        console.error(err);
+        throw err;
+    }
+
+    return printed;
+}
+
+function compileCode(checker: ts.TypeChecker, ctx: ts.TransformationContext, code: string) {
+    const runtimeFileName = `_generated_${Date.now()}.ts`;
+    const compilerHost = ts.createCompilerHost({
+        ...ctx.getCompilerOptions(),
+        strict: false,
+    });
+    const initialGetSourceFile = compilerHost.getSourceFile.bind(compilerHost);
+    const file = ts.createSourceFile(runtimeFileName, code, ctx.getCompilerOptions().target || ts.ScriptTarget.ES2018, false, ts.ScriptKind.TS);
+    const createdFiles = {};
+
+    compilerHost.getSourceFile = (fileName: string, ...args: any) => {
+        if (fileName === runtimeFileName) return file;
+        return initialGetSourceFile(fileName, ...args);
+    };
+    compilerHost.writeFile = (fileName: string, contents: string) => createdFiles[fileName] = contents;
+
+    const program = ts.createProgram([ file.fileName ], ctx.getCompilerOptions(), compilerHost);
+    program.emit(file, undefined, undefined, false, {
+        before: [
+            createSuperTransformer(checker, {})
+        ],
+    });
+
+    return Object.values(createdFiles)[0] as string;
+}
+
+function transform_comptime(
+    checker: ts.TypeChecker,
+    ctx: ts.TransformationContext,
+    funcBody: ts.ConciseBody,
+) {
+    const generatedFuncBody = extractComptimeCode(checker, ctx, funcBody);
+    const generatedFuncCode = `(($compiler) => ${generatedFuncBody})`;
+    return parseCodePickExpression(generatedFuncCode);
+}
+
+function run_transformed_comptime(
+    checker: ts.TypeChecker,
+    ctx: ts.TransformationContext,
+    code: string,
+    args: any[] = []
+) {
+    console.log('run code> ' + code + '\n----');
+    const compiledCode = compileCode(checker, ctx, code);
+    let generatedFunc;
+    let resultValue;
+
+    try {
+        generatedFunc = eval(compiledCode);
+    } catch (err) {
+        console.error('failed eval generated code:');
+        console.error(compiledCode);
+        console.error(err);
+        throw err;
+    }
+
+    try {
+        resultValue = generatedFunc(...args);
+    } catch (err) {
+        console.error('failed run generated func:');
+        console.error(compiledCode);
+        console.error(err);
+        throw err;
+    }
+
+    if (typeof resultValue === 'object') resultValue = JSON.stringify(resultValue);
+    return parseFuncBodyAndGenerateInvoke(`return (${resultValue})`);
+}
+
+function transform_compilerEval(checker: ts.TypeChecker, ctx: ts.TransformationContext, callExpr: ts.CallExpression) {
+    const $genericType = (i: number): ts.Type | undefined => {
+        if (callExpr.typeArguments) {
+            const targ = callExpr.typeArguments[i];
+            return checker.getTypeAtLocation(targ);
+        }
+        return undefined;
+    };
+    
+    const $argType = (i: number): ts.Type | undefined => {
+        if (callExpr.arguments) {
+            const targ = callExpr.arguments[i];
+            return checker.getTypeAtLocation(targ);
+        }
+        return undefined;
+    };
+
+    const $eachOfStrUnion = (cb: (value: string) => void, type?: ts.Type) => {
+        if (!type) type = $genericType(0);
+        if (type) {
+            if (type.isUnionOrIntersection()) {
+                for (const childT of type.types) {
+                    if (childT.isStringLiteral()) {
+                        cb(childT.value);
+                    }
+                }
+            }
+        }
+    };
+
+    const codeBody = unescapeText(callExpr.arguments[0].getFullText());
+    const codeFunc = eval('(() => {' + codeBody + '})');
+    const newCode = codeFunc();
+    const newNode = parseCode(newCode).statements;
+    return newNode as any;
+}
+
+const _jobApiUserEvents = new tsee.EventEmitter();
+
+function createCompilerJobAPI(params: {
+    checker: ts.TypeChecker,
+    ctx: ts.TransformationContext,
+    sourceFile: ts.SourceFile,
+    currentNode: ts.CallExpression,
+    setReplaceWith: (newNode: ts.Node) => void,
+}) {
+    // TODO: rewrite to static functions
+
+    return {
+        replaceWithCode: (code: string) => params.setReplaceWith(parseCodePickExpression(code)),
+        replaceWithNode: (astNode: any) => params.setReplaceWith(astNode),
+        compileCode: (code: string) => compileCode(params.checker, params.ctx, code),
+        extractComptimeCode_funcBody: (astFuncBodyNode: any) => extractComptimeCode(params.checker, params.ctx, astFuncBodyNode),
+        printCode: (astNode: any) => printCode(astNode, astNode.getSourceFile()),
+        parseCodePickExpression: (code: string) => parseCodePickExpression(code),
+        unescapeText: (text: string) => unescapeText(text),
+        // getTypeOf(symb: any): any;
+        getTypeOfGeneric: (genericTypeInd: number) => {
+            if (params.currentNode.typeArguments) {
+                const targ = params.currentNode.typeArguments[genericTypeInd];
+                const targType = params.checker.getTypeAtLocation(targ);
+
+                // getAccessibleSymbolChain
+                // console.log(targ.getFullText());
+
+                return targType;
+            }
+            return undefined;
+        },
+        getTypeChecker: () => params.checker,
+        getTransformContext: () => params.ctx,
+        visitEachChild: (astNode: any, visitor: any) => {
+            return ts.visitEachChild(astNode, visitor, params.ctx);
+        },
+        getTs: () => ts,
+        getCurrentNode: () => params.currentNode,
+        
+        addListener: (eventName: string, handler: any) => _jobApiUserEvents.addListener(eventName, handler),
+        removeListener: (eventName: string, handler: any) => _jobApiUserEvents.removeListener(eventName, handler),
+        emitEvent: (eventName: string, ...args: any) => _jobApiUserEvents.emit(eventName, ...args),
+    };
 }
 
 // use atomic waiter with separate thread to run async functions in sync transformer
 
-export default function(checker: ts.TypeChecker, pluginOptions: {}) {
+let _is_inside_comptime = 0;
+
+function createSuperTransformer(checker: ts.TypeChecker, pluginOptions: {}) {
     return (ctx: ts.TransformationContext) => {
         return (sourceFile: ts.SourceFile) => {
             function visitor(node: ts.Node): ts.VisitResult<ts.Node> {
-                if (ts.isImportDeclaration(node)) {
-                    const moduleName = importDecl_getModuleSpecifierName(node);
-                    if (moduleName === 'ts-transformer-overload') {
-                        // !!!!!!!
-                        // checker.getTypeOfSymbolAtLocation();
-                        // https://stackoverflow.com/questions/49139601/how-to-bind-an-identifier-with-an-existing-symbol-on-a-compiler-transformer-in-t
-
-
-                        // console.log([node.moduleSpecifier.getText()]);
-                        // import from ts-transformer-overload/lib
-                    }
-                }
-                if (ts.isDecorator(node)) {
-                    const { expression } = node;
-                    // console.log(node);
-                }
-                if (ts.isCallExpression(node) && unescapeText(node.expression.getText()) === 'comptime') {
-                    const arg0 = node.arguments[0];
-                    if (ts.isArrowFunction(arg0)) {
-                        const generatedFuncBody = extractComptimeCode(checker, ctx, arg0.body);
-                        const generatedFuncCode = `(() => ${generatedFuncBody})`;
-
-                        // console.log(generatedFuncCode);
-
-                        const runtimeFileName = `_generated_${Date.now()}.ts`;
-                        const compilerHost = ts.createCompilerHost({
-                            ...ctx.getCompilerOptions(),
-                            strict: false,
-                        });
-                        const initialGetSourceFile = compilerHost.getSourceFile.bind(compilerHost);
-                        const file = ts.createSourceFile(runtimeFileName, generatedFuncCode, ctx.getCompilerOptions().target, false, ts.ScriptKind.TS);
-                        const createdFiles = {};
-
-                        compilerHost.getSourceFile = (fileName: string, ...args: any) => {
-                            if (fileName === runtimeFileName) return file;
-                            return initialGetSourceFile(fileName, ...args);
-                        };
-                        compilerHost.writeFile = (fileName: string, contents: string) => createdFiles[fileName] = contents;
-
-                        const program = ts.createProgram([ file.fileName ], ctx.getCompilerOptions(), compilerHost);
-                        program.emit(file);
-
-                        const compiledCode = Object.values(createdFiles)[0] as string;
-                        // console.log(compiledCode);
-                        const generatedFunc = eval(compiledCode);
-
-                        return parseFuncBodyAndGenerateInvoke(`return (${generatedFunc()})`);
-                    }
-                }
                 if (ts.isExpressionStatement(node)) {
                     const child = node.expression;
 
-                    if (ts.isCallExpression(child) && unescapeText(child.expression.getText()) === 'compileTimeEval') {
-                        const $genericType = (i: number): ts.Type | undefined => {
-                            if (child.typeArguments) {
-                                const targ = child.typeArguments[i];
-                                return checker.getTypeAtLocation(targ);
-                            }
-                            return undefined;
-                        };
+                    if (ts.isCallExpression(child) && unescapeText(child.expression.getText()) === KEYWORD_COMPILER_EVAL) {
+                        return transform_compilerEval(checker, ctx, child);
+                    }
+                }
+                if (ts.isCallExpression(node) && unescapeText(node.expression.getText()) === KEYWORD_COMPTIME) {
+                    const arg0 = node.arguments[0];
+                    if (ts.isArrowFunction(arg0)) {
+                        ++_is_inside_comptime;
+                        const transformedNode = transform_comptime(checker, ctx, arg0.body);
+
+                        if (_is_inside_comptime === 1) {
+                            const transformedCode = printCode(transformedNode, sourceFile);
+                            const result = run_transformed_comptime(checker, ctx, transformedCode);
+                            --_is_inside_comptime;
+                            return result;
+                        }
+
+                        --_is_inside_comptime;
+                        return transformedNode;
+                    }
+                }
+                if (ts.isCallExpression(node) && unescapeText(node.expression.getText()) === KEYWORD_COMPILER_JOB) {
+                    const arg0 = node.arguments[0];
+                    if (ts.isArrowFunction(arg0)) {
+                        let replaceTo: ts.Node;
+
+                        const compilerJobApi = createCompilerJobAPI({
+                            checker,
+                            ctx,
+                            currentNode: node,
+                            sourceFile,
+                            setReplaceWith: (newNode) => replaceTo = newNode,
+                        });
+
+                        ++_is_inside_comptime;
+                        const transformedNode = transform_comptime(checker, ctx, arg0.body);
                         
-                        const $argType = (i: number): ts.Type | undefined => {
-                            if (child.arguments) {
-                                const targ = child.arguments[i];
-                                return checker.getTypeAtLocation(targ);
-                            }
-                            return undefined;
-                        };
+                        if (_is_inside_comptime === 1) {
+                            const transformedCode = printCode(transformedNode, sourceFile);
+                            const result = run_transformed_comptime(checker, ctx, transformedCode, [ compilerJobApi ]);
+                            --_is_inside_comptime;
 
-                        const $eachOfStrUnion = (cb: (value: string) => void, type?: ts.Type) => {
-                            if (!type) type = $genericType(0);
-                            if (type) {
-                                if (type.isUnionOrIntersection()) {
-                                    for (const childT of type.types) {
-                                        if (childT.isStringLiteral()) {
-                                            cb(childT.value);
-                                        }
-                                    }
-                                }
-                            }
-                        };
+                            if (replaceTo) return replaceTo;
+                            return result;
+                        }
 
-                        const codeBody = unescapeText(child.arguments[0].getFullText());
-                        const codeFunc = eval('(() => {' + codeBody + '})');
-                        const newCode = codeFunc();
-                        const newNode = parseCode(newCode).statements;
-                        return newNode as any;
+                        --_is_inside_comptime;
+                        return transformedNode;
                     }
                 }
                 return ts.visitEachChild(node, visitor, ctx);
@@ -255,3 +346,5 @@ export default function(checker: ts.TypeChecker, pluginOptions: {}) {
         };
     };
 }
+
+export default createSuperTransformer;
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
-// import { overload } from 'ts-transformer-overload';
+// -----------------------------------------------
+//                  definitions
 
-// declare const console: { log: any };
-
-// class A {
-//     foo() {
-//         return 10;
-//     }
-// }
-
-// class B extends A {
-//     @overload()
-//     foo() {
-//         return 20;
-//     }
-// }
-
-// const b = new B();
-
-// function magic(x: string) {
-//     return {} as any;
-// }
-
-function compileTimeEval<T>(x: string) {
+function __compilerEval<T>(x: string) {
     return {} as any;
 }
 
@@ -29,14 +9,76 @@ function comptime<T>(func: () => T): T {
     return func();
 }
 
-function comptimeStructs() {
-    type Variants = 'a' | 'b' | 'c' | 'e';
+type CompilerAPI = {
+    replaceWithCode<T = any>(code: string): T;
+    replaceWithNode<T = any>(astNode: any): T;
+    compileCode(code: string): string;
+    extractComptimeCode_funcBody(astFuncBodyNode: any): string;
+    printCode(astNode: any): string;
+    parseCodePickExpression(code: string): any;
+    unescapeText(text: string): string;
+    // getTypeOf(symb: any): any;
+    getTypeOfGeneric<T=any>(genericTypeInd: number): any;
+    getTypeChecker(): any;
+    getTransformContext(): any;
+    visitEachChild(astNode: any, visitor: any): any;
+    getTs(): any;
+    getCurrentNode(): any;
+    
+    addListener(eventName: string, handler: any): any;
+    removeListener(eventName: string, handler: any): any;
+    emitEvent(eventName: string, ...args: any): any;
+};
 
-    compileTimeEval<Variants>(`
+function __compilerJob<A1=any,A2=any,A3=any,A4=any,A5=any>(func: ($compiler: CompilerAPI) => void) {
+    return undefined!;
+}
+
+// -----------------------------------------------
+
+function compilerJobExample1() {
+    __compilerJob($compiler => {
+        let out = '';
+        const genericType = { types: [ { value: 'a' }, { value: 'b' } ] };
+        // walk over union
+        for (const childT of (genericType).types) {
+            out += childT.value + ': 0, ';
+        }
+
+        $compiler.replaceWithCode('({' + out + '})');
+    });
+}
+
+function compilerJobExample2<Names>(names: Names[]) {
+    __compilerJob<Names>($compiler => {
+        let out = '';
+        const genericType = $compiler.getTypeOfGeneric(0);
+        // walk over union
+        console.log(genericType);
+        if (genericType.isUnionOrIntersection()) {
+            for (const childT of genericType.types) {
+                if (childT.isStringLiteral()) {
+                    out += '"' + childT.value + '"';
+                }
+            }
+        }
+
+        console.log('result: ' + out);
+        $compiler.replaceWithCode('return ([ "test", ' + out + '])');
+    });
+}
+
+declare const Struct: any;
+
+function comptimeStructs() {
+
+    __compilerEval<'A'|'B'>(`
         let out = '';
         $eachOfStrUnion(x => out += x + ': 0, ');
         return 'const Struct = {' + out + '}';
     `);
+
+    return Struct;
 }
 
 function sum(a: number, b: number) {
@@ -57,3 +99,22 @@ function comptimeSum() {
 comptime(() => {
     return comptimeSum();
 });
+
+function compInComp() {
+    const result = comptime(() => {
+        return comptimeStructs();
+    });
+    console.log(result);
+}
+
+compInComp();
+
+function compApiInComptime() {
+    const result = comptime(() => {
+        console.log('compApiInComptime');
+        compilerJobExample2<'a'|'b'|'c'>([]);
+    });
+}
+
+// 
+// type.tryGetThisTypeAt(topCallStack)
